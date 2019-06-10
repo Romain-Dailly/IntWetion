89a40be5e8597273b90d86f175e0eb8030e6a145
Anais <anais.chevallier@gmail.com>
2019-06-10 18:43:14 +0200

    begining hook process/Form

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,37 +1,21 @@
 import React from 'react';
-import axios from 'axios';
-import React, {useState} from 'react';
+// import axios from 'axios';
+import { useState } from 'react';
 
 
 // Simon confirme qu'il faut passer par une fonction (pas de class) et utiliser les hook donc pas de controlled component ici.
 
 function Form() {
-// Custom Hook en chantier, version plus simple à venir, je m'y mets ce week end. Anaïs.
-  const useDataForm = (callback) => {
-    const [inputs, setInputs] = useState({});
-    const handleSubmit = (event) => {
-      if (event) {
-        event.preventDefault();
-      }
-    }
-    const handleInputChange = (event) => {
-      event.persist();
-      setInputs(inputs => ({...inputs, [event.target.name]: event.target.value}));
-    }
-    return {
-      handleSubmit,
-      handleInputChange,
-      inputs
-    };
-  }
 
-const handleInputChange = (event) => {
-  event.persist();
-  setInputs(inputs => ({...inputs, [event.target.name]: event.target.value}));
-}
+  // Je pense que c'est possible de faire seulement :
+  // const [adminInput, setAdminInput] = useState({});
+  const [adminInput, setAdminInput] = useState({
+    card: {},
+    questions: {},
+    quiz_ref: {},
+    video: {},
+  });
 
-
-  
   // () => {
   //   axios.post(`http://localhost:8080/card/`
   //   )} 
@@ -79,52 +63,54 @@ const handleInputChange = (event) => {
               <label for="exampleFormControlTextarea1">Type de carte :</label>
               <div>
                 <div class="form-check form-check-inline">
-                  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" />
+                  <input onClick={adminInput.card.online = 0} class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" />
                   <label class="form-check-label" for="inlineRadio1">En ligne - gratuit</label>
                 </div>
                 <div class="form-check form-check-inline">
-                  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" />
+                  <input onClick={adminInput.card.online} class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" />
                   <label class="form-check-label" for="inlineRadio2">En ligne - payant</label>
                 </div>
                 <div class="form-check form-check-inline">
-                  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3" />
+                  <input onClick={adminInput.card.online = 1} class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3" />
                   <label class="form-check-label" for="inlineRadio3">Non visible</label>
                 </div>
               </div>
             </div>
             <div class="form-group">
               <label for="exampleFormControlTextarea1">Description</label>
-              <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+              <textarea class="form-control" id="exampleFormControlTextarea1" value={adminInput.card.description} rows="3"></textarea>
             </div>
             <div class="form-group">
               <label for="exampleFormControlTextarea1">Lien, image de la carte</label>
-              <textarea class="form-control" id="exampleFormControlTextarea1" rows="1"></textarea>
+              <textarea value={adminInput.card.image_question} class="form-control" id="exampleFormControlTextarea1" rows="1"></textarea>
             </div>
             <div class="form-group">
-              <label for="exampleFormControlTextarea1">Lien, image de la carte</label>
-              <textarea class="form-control" id="exampleFormControlTextarea1" rows="1"></textarea>
+              <label for="exampleFormControlTextarea1">Lien, vidéo-intro de la carte</label>
+              <textarea value={adminInput.video.url_video} class="form-control" id="exampleFormControlTextarea1" rows="1"></textarea>
             </div>
             <div class="form-group">
-              <label for="exampleFormControlTextarea1">Lien, image de la carte</label>
-              <textarea class="form-control" id="exampleFormControlTextarea1" rows="1"></textarea>
+              <label for="exampleFormControlTextarea1">Lien, musique</label>
+              <textarea value={adminInput.video.url_video} class="form-control" id="exampleFormControlTextarea1" rows="1"></textarea>
             </div>
             <div class="form-group">
-              <label for="exampleFormControlTextarea1">Lien, image de la carte</label>
-              <textarea class="form-control" id="exampleFormControlTextarea1" rows="1"></textarea>
+              <label for="exampleFormControlTextarea1">Lien, vidéo, fin de test</label>
+              <textarea value={adminInput.url_video} class="form-control" id="exampleFormControlTextarea1" rows="1"></textarea>
             </div>
             <h1>Espace test</h1>
             <div class="form-group">
               <label for="formGroupExampleInput">Question</label>
               <div className="d-flex">
-                <input type="text" class="form-control mr-5" id="formGroupExampleInput" />
+                <input value={adminInput.questions.text_question} type="text" class="form-control mr-5" id="formGroupExampleInput" />
                 <button type="button" class="btn btn-secondary ">Supprimer</button>
               </div>
+              {/* Ajouter image_question */}
             </div>
             <div>
               <label class="form-check-label" for="inlineCheckbox1">Type de réponse liée</label>
               <fieldset class="form-group">
                 <div class="form-check form-check-inline">
-                  <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1" />
+                  {/* Faire un onClick ou mettre directement dans value ? */}
+                  <input onclick={adminInput.questions.type_response = 0} class="form-check-input" type="checkbox" id="inlineCheckbox1" value={adminInput.questions.type_response = 0} />
                   <label class="form-check-label" for="inlineCheckbox1">Numérique</label>
                 </div>
                 <div class="form-check form-check-inline">
@@ -168,7 +154,7 @@ const handleInputChange = (event) => {
                 </div>
               </div>
             </div>
-            <button onClick={() => sendData()} type="submit" class="btn btn-primary mt-5 mb-5" value="submit">Enregistrer</button>
+            <button onClick={() => setAdminInput({})} type="submit" class="btn btn-primary mt-5 mb-5" value="submit">Enregistrer</button>
           </form>
         </div>
       </div>
