d187407a192fb0e73b08118455d9a7b34e15aab3
Romain-Dailly <loops51@hotmail.com>
2019-06-13 10:33:07 +0200

    route get, post work in api.js

--- api/api.js
+++ api/api.js
@@ -10,20 +10,8 @@ app.use(cors({ origin: '*' }));
 app.use(bodyParser.json());
 // Support URL-encoded bodies
 app.use(bodyParser.urlencoded({
-  extended: true,
+  extended: false,
 }));
-
-// ROOT ROUTE GET
-app.get('/', (request, response) => {
-  connection.query('SELECT * FROM card', (error, results) => {
-    if (error) {
-      response.status(500).send('Erreur lors de la récupération des movies');
-      // console.log(error);
-    } else {
-      response.json(results);
-    }
-  });
-});
 
 // ROUTE card
 app.route('/card/')
@@ -33,56 +21,56 @@ app.route('/card/')
     const dataContentResources = data.resources;
     const dataContentQuestions = data.questions;
     const cardData = data.card;
+    
     // On poste le contenu de la table card
     connection.query('INSERT INTO card SET ?', cardData, (error, _) => {
       if (error) {
-        // console.log(error);
+        console.log(error);
         response.status(500).send("Erreur lors de l'ajout de la carte");
       }
     });
     // On récupère l'id de notre nouvelle card dans le resultat de la requête(resultId)
-    connection.query(`SELECT id FROM card WHERE name='${cardData.name}'`, (error, resultId) => {
-      if (error) {
-        // console.log(error);
+    connection.query(`SELECT id FROM card WHERE name='${cardData.name}'`, (errorId, resultId) => {
+      if (errorId) {
+        console.log(errorId);
         response.status(500).send("Erreur lors de la recuperation de l'id de la carte");
       }
       // On poste les videos(tableau d'objets) en injectant pour chacune notre resultId(reference)
-      dataContentVideos.map((dataContentVideo) => {
-        return connection.query(`INSERT INTO videos SET id_card ='${resultId[0].id}', ?`, dataContentVideo, (errorVideos, resultVideos) => {
+      dataContentVideos.map(dataContentVideo => {
+        connection.query(`INSERT INTO videos SET id_card ='${resultId[0].id}', ?`, dataContentVideo, (errorVideos, resultVideos) => {
           if (errorVideos) {
-            // console.log(error);
+            console.log(errorVideos);
             response.status(500).send("Erreur lors de l'ajout de la vidéo");
           }
         });
       });
       // On poste les ressources une à une(tableau d'objets)
       // en récupérant chaque id dans le resultResource
-      dataContentResources.map((dataContentResource) => {
-        return connection.query('INSERT INTO resources SET ?', dataContentResource, (errorResource, resultResource) => {
+      dataContentResources.map(dataContentResource => {
+        connection.query('INSERT INTO resources SET ?', dataContentResource, (errorResource, resultResource) => {
           if (errorResource) {
-            // console.log(error);
+            console.log(errorResource);
             response.status(500).send("Erreur lors de l'ajout de la ressource");
-          }
+          } 
           // Pour une ressource, on poste la question correspondante en injectant l'id de la ressource(resultResource); 
-          dataContentQuestions.map((dataContentQuestion) => {
-            return connection.query(`INSERT INTO questions SET id_resource=${resultResource.insertId}, ?`, dataContentQuestion, (errorQuestion, resultQuestion) => {
+          dataContentQuestions.map(dataContentQuestion => {
+            connection.query(`INSERT INTO questions SET id_resource=${resultResource.insertId}, ?`, dataContentQuestion, (errorQuestion, resultQuestion) => {
               if (errorQuestion) {
-                // console.log(error);
+                console.log(errorQuestion);
                 response.status(500).send("Erreur lors de l'ajout de la question");
               }
               // On lie la question ajoutée à la card en création par leurs id dans la table quiz_ref 
               connection.query(`INSERT INTO quiz_ref SET id_card=${resultId[0].id}, id_question=${resultQuestion.insertId}`, (errorQuiz, resultQuizRef) => {
                 if (errorQuiz) {
-                  // console.log(error);
+                  console.log(errorQuiz);
                   response.status(500).send("Erreur lors de l'ajout des id dans quiz_ref");
-                } else {
-                  response.sendStatus(200);
                 }
               });
             });
           });
         });
       });
+      response.sendStatus(200);
     })
   })
   .get((request, response) => {
