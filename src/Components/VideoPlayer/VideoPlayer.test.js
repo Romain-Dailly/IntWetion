cf3e77c0b557baea6cf66e3864092aacc8648752
ericafenyo <ericafenyo@yahoo.com>
2019-06-13 17:34:31 +0200

    code clean up

--- src/Components/VideoPlayer/VideoPlayer.test.js
+++ src/Components/VideoPlayer/VideoPlayer.test.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import VideoPlayer from './VideoPlayer'
+import VideoPlayer from './VideoPlayer';
 
 describe('Video Component', () => {
-    test('should be rendered without error ', () => {
-        mount(<VideoPlayer />);
-    });
-})
-
+  test('should be rendered without error ', () => {
+    mount(<VideoPlayer />);
+  });
+});
