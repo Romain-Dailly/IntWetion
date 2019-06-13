d3aaca7661a096ea8fbbbacf6b727f712f8f21aa
Romain-Dailly <loops51@hotmail.com>
2019-06-13 17:32:52 +0200

    on work on delete route

--- api/api.js
+++ api/api.js
@@ -11,8 +11,22 @@ app.use(cors({ origin: '*' }));
 app.use(bodyParser.json());
 // Support URL-encoded bodies
 app.use(bodyParser.urlencoded({
-  extended: false,
+  extended: true
 }));
+
+// ROOT ROUTE GET
+app.get('/', (request, response) => {
+  connection.query('SELECT * FROM card', (error, results) => {
+    if (error) {
+      response.status(500).send('Erreur lors de la récupération des movies');
+      console.log(error);
+    } else {
+      response.json(results);
+    }
+  });
+});
+
+// Echantillon test ---> bottom of page
 
 // ROUTE card
 app.route('/card/')
@@ -28,80 +42,108 @@ app.route('/card/')
         console.log(error);
         response.status(500).send("Erreur lors de l'ajout de la carte");
       }
-    });
-    // On récupère l'id de notre nouvelle card dans le resultat de la requête(resultId)
-    connection.query(`SELECT id FROM card WHERE name='${cardData.name}'`, (errorId, resultId) => {
-      if (errorId) {
-        console.log(errorId);
+    })
+    //On récupère l'id de notre nouvelle card dans le resultat de la requête(resultId)
+    connection.query(`SELECT id FROM card WHERE name='${cardData.name}'`, (error, resultId) => {
+      if (error) {
+        console.log(error);
         response.status(500).send("Erreur lors de la recuperation de l'id de la carte");
       }
-      // On poste les videos(tableau d'objets) en injectant pour chacune notre resultId(reference)
+      //On poste les videos(tableau d'objets) en injectant pour chacune notre resultId(reference)
       dataContentVideos.map(dataContentVideo => {
-        connection.query(`INSERT INTO videos SET id_card ='${resultId[0].id}', ?`, dataContentVideo, (errorVideos, resultVideos) => {
-          if (errorVideos) {
-            console.log(errorVideos);
+        return connection.query(`INSERT INTO videos SET id_card ='${resultId[0].id}', ?`, dataContentVideo, (error, resultVideo) => {
+          if (error) {
+            console.log(error);
             response.status(500).send("Erreur lors de l'ajout de la vidéo");
           }
         });
       });
-      // On poste les ressources une à une(tableau d'objets)
-      // en récupérant chaque id dans le resultResource
+      //On poste les ressources une à une(tableau d'objets)en récupérant chaque id dans le resultResource
       dataContentResources.map(dataContentResource => {
-        connection.query('INSERT INTO resources SET ?', dataContentResource, (errorResource, resultResource) => {
-          if (errorResource) {
-            console.log(errorResource);
+        return connection.query(`INSERT INTO resources SET ?`, dataContentResource, (error, resultResource) => {
+          if (error) {
+            console.log(error);
             response.status(500).send("Erreur lors de l'ajout de la ressource");
-          } 
-          // Pour une ressource, on poste la question correspondante en injectant l'id de la ressource(resultResource); 
-          dataContentQuestions.map(dataContentQuestion => {
-            connection.query(`INSERT INTO questions SET id_resource=${resultResource.insertId}, ?`, dataContentQuestion, (errorQuestion, resultQuestion) => {
-              if (errorQuestion) {
-                console.log(errorQuestion);
+          }
+          //Pour une ressource, on poste la question correspondante en injectant l'id de la ressource(resultResource) 
+          dataContentQuestions.map((dataContentQuestion) => {
+            return connection.query(`INSERT INTO questions SET id_resource=${resultResource.insertId}, ?`, dataContentQuestion, (error, resultQuestion) => {
+              if (error) {
+                console.log(error);
                 response.status(500).send("Erreur lors de l'ajout de la question");
               }
-              // On lie la question ajoutée à la card en création par leurs id dans la table quiz_ref 
-              connection.query(`INSERT INTO quiz_ref SET id_card=${resultId[0].id}, id_question=${resultQuestion.insertId}`, (errorQuiz, resultQuizRef) => {
-                if (errorQuiz) {
-                  console.log(errorQuiz);
+              //On lie la question ajoutée à la card en création par leurs id dans la table quiz_ref
+              connection.query(`INSERT INTO quiz_ref SET id_card=${resultId[0].id}, id_question=${resultQuestion.insertId}`, (error, resultQuiz_ref) => {
+                if (error) {
+                  console.log(error);
                   response.status(500).send("Erreur lors de l'ajout des id dans quiz_ref");
+                } else {
+                  response.sendStatus(200);
                 }
               });
             });
           });
         });
       });
-      response.sendStatus(200);
     })
   })
   .get((request, response) => {
-    connection.query('SELECT * FROM card', (error, result) => {
+    // On recupère l'id de la card envoyé en paramètre du fetch en front
+    let idCard = request.body.id;
+    // On récupère le contenu de la table card grâce à l'id
+    connection.query(`SELECT * FROM card WHERE id=${idCard}`, (error, result) => {
       if (error) {
-        // console.log(error);
-        response.status(500).send("Erreur lors de la récupération de la carte");
+        console.log(error);
+        response.status(500).send("Erreur lors de la récupération de la carte")
       }
-      connection.query(`SELECT * FROM videos WHERE id_card=${result[0].id}`, (error, resultVideo) => {
+      // Puis le contenu de la table vidéo correspondant à l'id
+      connection.query(`SELECT * FROM videos WHERE id_card=${idCard}`, (error, resultVideo) => {
         if (error) {
-          // console.log(error);
-          response.status(500).send('Erreur lors de la récupération des vidéos');
+          console.log(error);
+          response.status(500).send("Erreur lors de la récupération des vidéos");
         }
+        // Celui de la table quiz_ref, questions et resources lié à l'id
         connection.query(`SELECT * FROM quiz_ref q JOIN questions ON questions.id = id_question 
-        JOIN resources r ON r.id = questions.id_resource WHERE q.id_card=${result[0].id}`, (error, resultQuestions) => {
+        JOIN resources r ON r.id = questions.id_resource WHERE q.id_card=${idCard}`, (error, resultQuestions) => {
             if (error) {
-              // console.log(error);
+              console.log(error);
               response.status(500).send("Erreur lors de la récupération des questions")
             } else {
+              // On créée un objet contenant les resultats des queries sur chaques tables {card : [{}], videos : [{}], ...}
               const data = {
                 card: result,
                 videos: resultVideo,
-                questions: resultQuestions,
-              };
+                questions: resultQuestions
+              }
               response.status(200).send(data);
             }
-          });
+          })
+      });
+    });
+  })
+  .delete((request, response) => {
+    let idCard = request.body.id;
+    connection.query(`DELETE FROM card WHERE id=${idCard}`, (error, result) => {
+      if (error) {
+        console.log(error);
+        response.status(500).send("Erreur lors de la suppression de la carte")
+      }
+      connection.query(`DELETE FROM videos WHERE id_card=${idCard}`, (error, resultVideo) => {
+        if (error) {
+          console.log(error);
+          response.status(500).send("Erreur lors de la suppression des vidéos");
+        }
+        connection.query(`DELETE FROM quiz_ref q JOIN questions ON questions.id = id_question 
+        JOIN resources r ON r.id = questions.id_resource WHERE q.id_card=${idCard}`, (error, resultQuestions) => {
+            if (error) {
+              console.log(error);
+              response.status(500).send("Erreur lors de la suppression des questions")
+            }
+            response.sendStatus(200);
+          })
       });
     });
   });
-
 app.listen(port, (req, res) => {
   console.log(`listening on port ${port}`);
 });
