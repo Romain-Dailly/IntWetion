3ac3478c04997fda4bfce45de0d9240b499193ba
Romain-Dailly <loops51@hotmail.com>
2019-06-17 11:17:50 +0200

    added dir SoundPlayer

---
+++ src/Components/Test/SoundPlayer/SoundPlayer.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+export default function SoundPlayer () {
+
+  const [volume, setVolume] = useState(0.5)
+
+  return(
+    <div className="volumeAll">
+      <div>
+      
+
+      </div>
+    </div>
+  )
+}
