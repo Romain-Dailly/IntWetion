0994376d70131ce0f96e952b09cf9c1ba805df3e
Romain-Dailly <loops51@hotmail.com>
2019-06-18 14:28:13 +0200

    eslint!

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import React from 'react';
 import './App.css';
 import Form from './Components/Form/Form';
 import ModalTest from './Components/Test/ModalTest/ModalTest';
-import SoundPlayer from './Components/Test/SoundPlayer/SoundPlayer';
 
 
 function App() {
 
   return (
     <div>
-      <SoundPlayer/>
-      {/* <Form /> 
-      <ModalTest /> */}
+      <Form /> 
+      <ModalTest />
     </div>
   );
 }
--- src/Components/Test/SoundPlayer/SoundPlayer.js
+++ src/Components/Test/SoundPlayer/SoundPlayer.js
@@ -1,41 +1,45 @@
 import React, { useState, useEffect } from 'react';
 import './SoundPlayer.css';
 import ReactPlayer from 'react-player';
-import axios from 'axios';
+// import axios from 'axios';
+
 
 function SoundPlayer() {
 
-  const [volume, setVolume] = useState(0.8)
-  const [showSlider, setShowSlider] = useState(false)
-  const [url, setUrl] = useState("")
+  const [volume, setVolume] = useState(0.8);
+  const [showSlider, setShowSlider] = useState(false);
+  // const [url, setUrl] = useState('');
 
   useEffect(() => {
-    axios.get('http://localhost:8080/card/', { params: { id: 1 } })
-      .then(result => setUrl((result.data.videos.filter(vid => {
-        return vid.type_video === 2 ? vid.url_video : null;
-      })[0].url_video))
-      )
-  }, [])
+  //   axios.get('http://localhost:8080/card/', { params: { id: 1 } })
+  //     .then(result => 
+  //       setUrl((result.data.videos.filter(vid => 
+  //         vid.type_video === 2 ? vid.url_video : null
+  //       )[0].url_video))
+  //     )
+  }, []);
 
   let style = showSlider ? { color: 'rgb(4, 4, 185)' } : { color: 'blue' };
 
   const volumeHandleClick = () => {
-    setShowSlider(!showSlider)
+    setShowSlider(!showSlider);
     showSlider ? style = { color: 'rgb(4, 4, 185)' } : style = { color: 'blue' };
-  }
+  };
   return (
     <div>
       <div className="player">
         <ReactPlayer
-          url={url}
+          url={}
           playing
           volume={volume}
         />
       </div>
       <div className="volumeAll d-flex justify-content-center">
-        {showSlider ?
-          <div className="sliderwrapper">
-            <input type="range"
+        {showSlider 
+        ?
+          (<div className="sliderwrapper">
+            <input 
+              type="range"
               min="0"
               max="1"
               step="any"
@@ -43,7 +47,7 @@ function SoundPlayer() {
               onChange={(e) => setVolume(e.target.value)}
               className="form-control-range"
             />
-          </div>
+          </div>)
           : null}
         <div className="soundIcon"  >
           <i className='fas fa-volume-down' style={style} onClick={() => volumeHandleClick()} title="Modifier le volume"></i>
@@ -52,5 +56,6 @@ function SoundPlayer() {
     </div>
   )
 }
+
 
 export default SoundPlayer;
