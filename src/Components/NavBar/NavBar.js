1209d52f758184fe385cbc71d3e19b9eb0311c1b
ericafenyo <ericafenyo@yahoo.com>
2019-06-18 14:44:44 +0200

    add NavBar component

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import './NavBar.css';
+
+const NavBar = ({ title }) => {
+  const [isDrawerOpen, openDrawer] = useState(false);
+
+  return (
+    <header>
+      <div className="nav-bar">
+        <i className="icon icon-arrow-back" />
+        <p className="title m-0">{title}</p>
+        <i
+          onClick={() => openDrawer(!isDrawerOpen)}
+          className={`icon icon-${isDrawerOpen ? 'close' : 'menu'}`}
+          role="button"
+          tabIndex="0"
+        />
+      </div>
+      <nav className={`main-nav ${isDrawerOpen ? 'active' : ''}`}>
+        <ul>
+          <li><a href="/">Accueil</a></li>
+          <li><a href="/">Services</a></li>
+          <li><a href="/">Team</a></li>
+          <li><a href="/">About</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+NavBar.propTypes = {
+  title: PropTypes.string,
+};
+
+export default NavBar;
