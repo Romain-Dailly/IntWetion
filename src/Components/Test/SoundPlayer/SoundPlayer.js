c8f1f1c6f152861aa0f42461e40d3d7a602f90b1
Romain-Dailly <loops51@hotmail.com>
2019-06-21 15:23:52 +0200

    soundplayer eslint

--- src/Components/Test/SoundPlayer/SoundPlayer.js
+++ src/Components/Test/SoundPlayer/SoundPlayer.js
@@ -1,61 +1,53 @@
 import React, { useState, useEffect } from 'react';
 import './SoundPlayer.css';
 import ReactPlayer from 'react-player';
-// import axios from 'axios';
-
 
 function SoundPlayer() {
-
   const [volume, setVolume] = useState(0.8);
   const [showSlider, setShowSlider] = useState(false);
   const [url, setUrl] = useState('');
 
   useEffect(() => {
-    // axios.get('http://localhost:8080/card/', { params: { id: 1 } })
-    //   .then(result => 
-    //     setUrl((result.data.videos.filter(vid => 
-    //       vid.type_video === 2 ? vid.url_video : null
-    //     )[0].url_video))
-    //   )
+    setUrl('http');
   }, []);
 
   let style = showSlider ? { color: 'rgb(4, 4, 185)' } : { color: 'blue' };
 
   const volumeHandleClick = () => {
     setShowSlider(!showSlider);
-    showSlider ? style = { color: 'rgb(4, 4, 185)' } : style = { color: 'blue' };
+    style = showSlider ? { color: 'rgb(4, 4, 185)' } : { color: 'blue' };
   };
   return (
     <div>
       <div className="player">
         <ReactPlayer
-          url={}
+          url={url}
           playing
           volume={volume}
         />
       </div>
       <div className="volumeAll d-flex justify-content-center">
-        {showSlider 
-        ?
-          (<div className="sliderwrapper">
-            <input 
-              type="range"
-              min="0"
-              max="1"
-              step="any"
-              value={volume}
-              onChange={(e) => setVolume(e.target.value)}
-              className="form-control-range"
-            />
-          </div>)
+        {showSlider
+          ? (
+            <div className="sliderwrapper">
+              <input
+                type="range"
+                min="0"
+                max="1"
+                step="any"
+                value={volume}
+                onChange={e => setVolume(e.target.value)}
+                className="form-control-range"
+              />
+            </div>
+          )
           : null}
-        <div className="soundIcon"  >
-          <i className='fas fa-volume-down' style={style} onClick={() => volumeHandleClick()} title="Modifier le volume"></i>
+        <div className="soundIcon">
+          <i className="fas fa-volume-down" tabIndex="0" role="button" style={style} onClick={() => volumeHandleClick()} title="Modifier le volume" />
         </div>
       </div>
     </div>
-  )
+  );
 }
-
 
 export default SoundPlayer;
