99c769b7a567d24a88ee9a7da68638f53d509671
ericafenyo <ericafenyo@yahoo.com>
2019-06-24 07:00:34 +0200

    add test presentations

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -1,13 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import '../NavBar/NavBar.css';
+import Question from '../Question/Question';
 import './Modal.css';
-
-const ContextToolBar = ({ title }) => (
-  <div className="nav-bar context-tool-bar">
-    <p className="title m-0">{title}</p>
-  </div>
-);
 
 const ActionBar = ({ closeModal, onNextButtonClick }) => (
   <div className="nav-bar action-bar">
@@ -16,18 +11,43 @@ const ActionBar = ({ closeModal, onNextButtonClick }) => (
   </div>
 );
 
-const Modal = ({ title, children, color = 'white' }) => {
-  const [isVisible, setVisibility] = useState(false);
+const Modal = ({ questions, color = 'white',quitQuiz }) => {
+  const [answers, setAnswers] = useState({});
+  const [questionIndex, setQuestionIndex] = useState(0);
+
+  const nextQuestion = () => {
+    const questionCount = questions.length - 1;
+    if (questionIndex < questionCount) {
+      setQuestionIndex(questionIndex + 1);
+    }
+  }
+
+  const getProgress = () => {
+    const numberOfQuestion = questions.length - 1;
+    return (questionIndex) / numberOfQuestion * 100
+  }
+  const storeAnswer = (answer, number) => {
+    const questionKey = `question-${number}`;
+    const answersCopy = answers;
+    answersCopy[questionKey] = answer;
+    setAnswers(answersCopy);
+  }
 
   return (
     <div className="overlay">
       <div className="overlay-content" style={{ background: `${color}` }}>
-        <ContextToolBar title="Administrator" />
-        <div className="content">{children}</div>
+        <div className="ui-progress">
+          <div className="ui-progress-bar" role="progressbar" style={{ width: `${getProgress()}%` }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+        </div>
+        <div className="content">
+          <Question
+            question={questions[questionIndex]}
+            onAnswerSelected={storeAnswer}
+          />
+        </div>
         <ActionBar
-          title={title}
-          onNextButtonClick={() => setVisibility(isVisible)}
-          closeModal={setVisibility(true)}
+          onNextButtonClick={nextQuestion}
+          closeModal={quitQuiz}
         />
       </div>
     </div>
---
+++ src/Components/Question/Question.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Question.css'
+
+const Question = ({ question, onAnswerSelected }) => {
+  const { text, image } = question;
+
+  const RadioButton = ({ number }) => {
+    return (
+      <div className="radio-button">
+        <input className="choice-radio" type="radio" name="answer-radio" onChange={() => onAnswerSelected(number, question.number)} />
+        <label className="check-mark m-0"></label>
+        <span>{number}</span>
+      </div>);
+  }
+
+  const answerRange = [...Array(11).keys()];
+
+  return (
+    <div className="question my-5">
+      <div className="container">
+        <img src={image} alt="" width="100%" />
+        <p className="body-1 my-3">{text}</p>
+        <div role="radiogroup" className="radio-button-wrapper">
+          {answerRange.map(number => <RadioButton key={number} number={number} />)}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+Question.prototype = {
+  question: PropTypes.string,
+};
+
+export default Question;
