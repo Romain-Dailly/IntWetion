cd53779d9c5bdfd4521ace25effed939062c796a
Anais <anais.chevallier@gmail.com>
2019-06-24 17:58:52 +0200

    hook synchronisation / modal question  creation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Form from './Components/adminpanel/Form/Form';
+import AdminQuestion from './Components/adminpanel/AdminQuestion';
 
 function App() {
   return (
     <div>
-      <Form />
+
+     <AdminQuestion />
     </div>
   );
 }
