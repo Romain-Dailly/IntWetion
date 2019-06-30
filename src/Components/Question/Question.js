673be9c2b6db841910a02ad73b03f46b6397aed4
ericafenyo <ericafenyo@yahoo.com>
2019-06-30 17:07:46 +0200

    tweak radio button to a rectangular shape

--- src/Components/Question/Question.js
+++ src/Components/Question/Question.js
@@ -1,36 +1,46 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './Question.css'
+import React from "react";
+import PropTypes from "prop-types";
+import "./Question.css";
 
 const Question = ({ question, onAnswerSelected }) => {
   const { text, image } = question;
 
   const RadioButton = ({ number }) => {
     return (
-      <div className="radio-button">
-        <input className="choice-radio" type="radio" name="answer-radio" onChange={() => onAnswerSelected(number, question.number)} />
-        <label className="check-mark m-0"></label>
-        <span>{number}</span>
-      </div>);
-  }
+      <div className="col-4">
+        <div className="radio-button mb-4">
+          <input
+            className="choice-radio"
+            type="radio"
+            name="answer-radio"
+            onChange={() => onAnswerSelected(number, question.number)}
+          />
+          <label className="check-mark m-0" />
+          <span>{number}</span>
+        </div>
+      </div>
+    );
+  };
 
-  const answerRange = [...Array(11).keys()];
+  const answerRange = [1, 2, 3, 4, 6, 7, 8, 9, 0];
 
   return (
-    <div className="question my-5">
+    <div className="question">
       <div className="container">
         <img src={image} alt="" width="100%" />
         <p className="body-1 my-3">{text}</p>
-        <div role="radiogroup" className="radio-button-wrapper">
-          {answerRange.map(number => <RadioButton key={number} number={number} />)}
+        <div className="row justify-content-center">
+          {answerRange.map(number => (
+            <RadioButton key={number} number={number} />
+          ))}
         </div>
       </div>
     </div>
   );
-}
+};
 
 Question.prototype = {
-  question: PropTypes.string,
+  question: PropTypes.string
 };
 
 export default Question;
