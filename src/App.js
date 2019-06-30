b093fd3677e37da018c50ea315f53346ab0eede2
ericafenyo <ericafenyo@yahoo.com>
2019-07-01 00:50:33 +0200

    add media queries for desktop

--- src/App.js
+++ src/App.js
@@ -1,80 +1,87 @@
-import React, { useState, Fragment } from 'react';
-import Modal from './Components/Modal/Modal';
-import NavBar from './Components/NavBar/NavBar'
-import './App.css';
-import Home from './Components/Home/Home';
+import React, { useState, Fragment } from "react";
+import Modal from "./Components/Modal/Modal";
+import NavBar from "./Components/NavBar/NavBar";
+import "./App.css";
+import Home from "./Components/Home/Home";
 
 const data = [
   {
     overline: "Flash",
     title: "Force",
-    description: "The sacral chakra is all about our connection and ability to accept others and new experiences.",
-    image: "https://images.unsplash.com/photo-1516526995003-435ccce2be97?w=800",
+    description:
+      "The sacral chakra is all about our connection and ability to accept others and new experiences.",
+    image: "https://images.unsplash.com/photo-1516526995003-435ccce2be97?w=800"
   },
   {
     overline: "Flash",
     title: "Peur",
-    description: "The sacral chakra is all about our connection and ability to accept others and new experiences.",
-    image: "https://images.unsplash.com/photo-1504021624863-054aa77f753f?w=800",
+    description:
+      "The sacral chakra is all about our connection and ability to accept others and new experiences.",
+    image: "https://images.unsplash.com/photo-1504021624863-054aa77f753f?w=800"
   },
 
   {
     overline: "Temporelles",
     title: "Terrestre",
-    description: "The sacral chakra is all about our connection and ability to accept others and new experiences.",
-    image: "https://images.unsplash.com/photo-1539131729610-2b4d9cf62e2c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2125&q=80",
+    description:
+      "The sacral chakra is all about our connection and ability to accept others and new experiences.",
+    image:
+      "https://images.unsplash.com/photo-1539131729610-2b4d9cf62e2c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2125&q=80"
   }
-]
+];
 
 const questions = [
   {
     number: 1,
-    image: "https://images.unsplash.com/photo-1453738773917-9c3eff1db985?w=1000",
+    image:
+      "https://images.unsplash.com/photo-1453738773917-9c3eff1db985?w=1000",
     text: "Is your voice loud and clear?"
   },
   {
     number: 2,
-    image: "https://images.unsplash.com/photo-1525120334885-38cc03a6ec77?w=1000",
+    image:
+      "https://images.unsplash.com/photo-1525120334885-38cc03a6ec77?w=1000",
     text: "Are you withdrawn or lonely, or do you keep people at a distance?"
   },
   {
     number: 3,
-    image: "https://images.unsplash.com/photo-1516544820488-4a99efa70a58?w=1000",
+    image:
+      "https://images.unsplash.com/photo-1516544820488-4a99efa70a58?w=1000",
     text: "Do you regularly avoid particular situations?"
   },
   {
     number: 4,
-    image: "https://images.unsplash.com/photo-1536602012356-86c345795580?w=1000",
+    image:
+      "https://images.unsplash.com/photo-1536602012356-86c345795580?w=1000",
     text: "Do you have visions or premonitions?"
   },
   {
     number: 5,
-    image: "https://images.unsplash.com/photo-1530210124550-912dc1381cb8?w=1000",
+    image:
+      "https://images.unsplash.com/photo-1530210124550-912dc1381cb8?w=1000",
     text: "Are you good at thinking in words, symbols and concepts?"
-  },
-]
+  }
+];
 
 function App() {
   const [isVisible, setVisibility] = useState(false);
   const runQuiz = () => {
     setVisibility(true);
-  }
+  };
 
   const quitQuiz = () => {
     console.log("quit");
-
     setVisibility(false);
-  }
+  };
 
   return (
     <Fragment>
       <NavBar title="Forces" />
       <Home cards={data} runQuiz={runQuiz} />
-      <div className={(isVisible ? "" : "d-none")}>
+      <div className={isVisible ? "" : "d-none"}>
         <Modal questions={questions} quitQuiz={quitQuiz} />
       </div>
     </Fragment>
-
   );
 }
 
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,36 +1,60 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import './NavBar.css';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import "./NavBar.css";
 
-const NavBar = ({ title }) => {
+const NavBar = () => {
   const [isDrawerOpen, openDrawer] = useState(false);
-
   return (
     <header className="main-header">
       <div className="nav-bar">
-        <i className="icon icon-arrow-back" />
-        <p className="title m-0">{title}</p>
+        <a className="logo mr-3">
+          <i className="icon-alt icon-lotus mr-2" />
+          <p className="m-0">Intwetion</p>
+        </a>
         <i
           onClick={() => openDrawer(!isDrawerOpen)}
-          className={`icon icon-${isDrawerOpen ? 'close' : 'menu'}`}
+          className={`icon icon-${isDrawerOpen ? "close" : "menu"} d-lg-none`}
           role="button"
           tabIndex="-1"
         />
+        <nav className={`main-nav ${isDrawerOpen ? "active" : ""}`}>
+          <ul>
+            <li>
+              <a href="/">Présentation</a>
+            </li>
+            <li>
+              <a href="/">Libérer l'intuition</a>
+            </li>
+            <li className="carte-de-vie d-flex justify-content-center">
+              <a href="/">Cartes de vie</a>
+              <i className="icon-alt icon-chevron-down" />
+              <ul className="carte-de-vie-child">
+                <li>
+                  <a href="/">Flash</a>
+                </li>
+                <li>
+                  <a href="/">Temporelles</a>
+                </li>
+                <li>
+                  <a href="/">A la "Carte"</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href="/">Bien-être et musique</a>
+            </li>
+            <li>
+              <i className="icon-alt icon-more-horiz" />
+            </li>
+          </ul>
+        </nav>
       </div>
-      <nav className={`main-nav ${isDrawerOpen ? 'active' : ''}`}>
-        <ul>
-          <li><a href="/">Accueil</a></li>
-          <li><a href="/">Services</a></li>
-          <li><a href="/">Team</a></li>
-          <li><a href="/">About</a></li>
-        </ul>
-      </nav>
     </header>
   );
 };
 
 NavBar.propTypes = {
-  title: PropTypes.string,
+  title: PropTypes.string
 };
 
 export default NavBar;
