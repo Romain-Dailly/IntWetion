9a6619eb4cd0abfc0d60a8495710ed1ee0aea02a
ericafenyo <ericafenyo@yahoo.com>
2019-06-30 17:09:19 +0200

    add a new componet and enhance general looks

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -1,17 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import '../NavBar/NavBar.css';
-import Question from '../Question/Question';
-import './Modal.css';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import "../NavBar/NavBar.css";
+import Question from "../Question/Question";
+import "./Modal.css";
 
 const ActionBar = ({ closeModal, onNextButtonClick }) => (
   <div className="nav-bar action-bar">
-    <button type="button" className="button" onClick={closeModal}>Close</button>
-    <button type="button" className="button" onClick={onNextButtonClick}>Next</button>
+    <button type="button" className="button" onClick={closeModal}>
+      Close
+    </button>
+    <button type="button" className="button" onClick={onNextButtonClick}>
+      Next
+    </button>
   </div>
 );
 
-const Modal = ({ questions, color = 'white',quitQuiz }) => {
+/**
+ *
+ * @param {object} props
+ */
+const Modal = ({ questions, color = "white", quitQuiz }) => {
   const [answers, setAnswers] = useState({});
   const [questionIndex, setQuestionIndex] = useState(0);
 
@@ -20,35 +28,51 @@ const Modal = ({ questions, color = 'white',quitQuiz }) => {
     if (questionIndex < questionCount) {
       setQuestionIndex(questionIndex + 1);
     }
-  }
+  };
 
   const getProgress = () => {
     const numberOfQuestion = questions.length - 1;
-    return (questionIndex) / numberOfQuestion * 100
-  }
+    return (questionIndex / numberOfQuestion) * 100;
+  };
+
   const storeAnswer = (answer, number) => {
     const questionKey = `question-${number}`;
     const answersCopy = answers;
     answersCopy[questionKey] = answer;
     setAnswers(answersCopy);
-  }
+  };
+
+  const ToolBar = ({ title }) => {
+    return (
+      <div className="context-tool-bar background-white">
+        <i className="logo icon-alt icon-lotus" />
+        <p className="header-5 m-0">{title}</p>
+        <i className="icon icon-close" />
+      </div>
+    );
+  };
 
   return (
     <div className="overlay">
       <div className="overlay-content" style={{ background: `${color}` }}>
-        <div className="ui-progress">
-          <div className="ui-progress-bar" role="progressbar" style={{ width: `${getProgress()}%` }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-        </div>
+        <ToolBar title="Forces" />
         <div className="content">
           <Question
             question={questions[questionIndex]}
             onAnswerSelected={storeAnswer}
           />
         </div>
-        <ActionBar
-          onNextButtonClick={nextQuestion}
-          closeModal={quitQuiz}
-        />
+        <div className="ui-progress">
+          <div
+            className="ui-progress-bar"
+            role="progressbar"
+            style={{ width: `${getProgress()}%` }}
+            aria-valuenow="25"
+            aria-valuemin="0"
+            aria-valuemax="100"
+          />
+        </div>
+        <ActionBar onNextButtonClick={nextQuestion} closeModal={quitQuiz} />
       </div>
     </div>
   );
@@ -57,7 +81,7 @@ const Modal = ({ questions, color = 'white',quitQuiz }) => {
 Modal.propTypes = {
   title: PropTypes.string,
   children: PropTypes.element,
-  color: PropTypes.string,
+  color: PropTypes.string
 };
 
 export default Modal;
