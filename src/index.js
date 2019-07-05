63d94035f0b5d2cb1e2c324b1c1a487c1891431b
ericafenyo <ericafenyo@yahoo.com>
2019-07-05 15:51:41 +0200

    created bug

--- src/index.js
+++ src/index.js
@@ -1,25 +1,34 @@
 import React from "react";
+import { createBrowserHistory } from "history";
 import ReactDOM from "react-dom";
-import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+import { ConnectedRouter, push } from "connected-react-router";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter
+} from "react-router-dom";
 import { Provider } from "react-redux";
 
 import "./css/index.css";
-import reducers from "./reducers";
+import rootReducer from "./reducers";
 import App from "./App";
 
+export const history = createBrowserHistory();
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
-  combineReducers({
-    ...reducers
-  }),
+  rootReducer(history),
   composeEnhancers(applyMiddleware(thunk))
 );
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
   </Provider>,
   document.getElementById("root")
 );
