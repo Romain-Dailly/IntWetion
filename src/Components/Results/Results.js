d4c1773af21a4c966d24cbe66afd514d34df3384
Anais <anais.chevallier@gmail.com>
2019-07-05 15:53:28 +0200

    step 1, result component

--- src/Components/Results/Results.js
+++ src/Components/Results/Results.js
@@ -1,5 +1,20 @@
 import React from 'react';
+import './Results.css';
 
-const Results = () => <div>Results</div>;
+const Results = () => {
+  return (
+    <div className="container-result">
+      <h5>Souhaitez-vous développer une de ces forces ?</h5>
+      <div className="card-results">
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item" >BHMZHFKZLGEFYELUZGFEJLZGFHMZGFHL</li>
+          <li className="list-group-item" >BHMZHFKZLGEFYELUZGFEJLZGFHMZGFHL</li>
+          <li className="list-group-item" >BHMZHFKZLGEFYELUZGFEJLZGFHMZGFHL</li>
+        </ul>
+      </div>
+    </div>
+
+  )
+};
 
 export default Results;
