0defab15b01ba87f58f6af5794258457c47ae89f
Anais <anais.chevallier@gmail.com>
2019-07-09 11:15:41 +0200

    maping of the 3 questions which have minimal scoring

--- src/Components/Results/Results.js
+++ src/Components/Results/Results.js
@@ -1,25 +1,69 @@
 import React from 'react';
-import {useSelector} from 'react-redux';
+// import {useSelector} from 'react-redux';
 
 import './Results.css';
+// import { returnStatement } from '@babel/types';
+// import { log } from 'util';
 
-const Results = () => {
-  const selectData = useSelector(store=> store.router.location.state);
-  console.log(selectData);
-  
+//Appel du state via le store redux
+// const Results = () => {
+//   const selectData = useSelector(store=> store.router.location.state);
+//   console.log(selectData);
+function Results() {
+  const answers = [
+    {
+      id: 1,
+      score: 1,
+      text: "la première"
+    },
+    {
+      id: 2,
+      score: 2,
+      text: "la deuxième"
+    },
+    {
+      id: 3,
+      score: 0,
+      text: "la première"
+    },
+    {
+      id: 4,
+      score: 9,
+      text: "la quatrième"
+    },
+    {
+      id: 5,
+      score: 6,
+      text: "la sixième"
+    },
+    {
+      id: 6,
+      score: 8,
+      text: "la sixième"
+    },
+  ]
+
+const getQuestions = () => {
+  const getMinScore = answers.sort((a, b) => {
+    return a.score -b.score;
+  })
+  return [getMinScore[0], getMinScore[1], getMinScore[2]]
+}
+
   return (
     <div className="container-result">
       <h5>Souhaitez-vous développer une de ces forces ?</h5>
       <div className="card-results">
         <ul className="list-group list-group-flush">
-          <li className="list-group-item" >Ma confiance.</li>
-          <li className="list-group-item" >Mes rêves.</li>
-          <li className="list-group-item" >Mon état d'être.</li>
+       {getQuestions().map((question)=> {
+        return <li className="list-group-item" >{question.text}</li>
+       })}
         </ul>
       </div>
     </div>
 
   )
 };
+
 
 export default Results;
