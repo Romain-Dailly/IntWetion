53a10c263b024c6c739f76d4b2ff14015414e732
Anais <anais.chevallier@gmail.com>
2019-07-10 13:54:28 +0200

    cleaning results component for eslint

--- src/Components/Results/Results.js
+++ src/Components/Results/Results.js
@@ -1,203 +1,202 @@
 import React, { useState } from 'react';
 // import {useSelector} from 'react-redux';
 
-
 import './Results.css';
 
 // import { returnStatement } from '@babel/types';
 // import { log } from 'util';
 
-//Appel du state via le store redux
+// Appel du state via le store redux
 // const Results = () => {
-//   const selectData = useSelector(store=> store.router.location.state);
-//   console.log(selectData);
+// const selectData = useSelector(store=> store.router.location.state);
+// console.log(selectData);
 
 // const [modal, setModal] = useState(false);
-
 
 const Results = () => {
   const answers = [
     {
       id: 1,
       score: 1,
-      text: "la première question",
+      text: 'la première question',
       resources: [
         {
-          "url_resource": "blabla",
-          "type_resource": "2"
+          url_resource: 'blabla',
+          type_resource: '2',
         },
         {
-          "url_resource": "blabla2",
-          "type_resource": "2"
-        }
-      ]
+          url_resource: 'blabla2',
+          type_resource: '2',
+        },
+        {
+          url_resource: 'blabla2',
+          type_resource: '1',
+        },
+      ],
     },
     {
       id: 2,
       score: 2,
-      text: "la deuxième question",
+      text: 'la deuxième question',
       resources: [
         {
-          "url_resource": "blabla",
-          "type_resource": "3"
+          url_resource: 'blabla',
+          type_resource: '3',
         },
         {
-          "url_resource": "blabla2",
-          "type_resource": "3"
-        }
-      ]
+          url_resource: 'blabla2',
+          type_resource: '3',
+        },
+      ],
     },
     {
       id: 3,
       score: 0,
-      text: "la troisème question",
+      text: 'la troisème question',
       resources: [
         {
-          "url_resource": "blabla",
-          "type_resource": "1"
+          url_resource: 'blabla',
+          type_resource: '1',
         },
         {
-          "url_resource": "blabla2",
-          "type_resource": "1"
-        }
+          url_resource: 'blabla2',
+          type_resource: '1',
+        },
+        {
+          url_resource: 'blabla2',
+          type_resource: '3',
+        },
       ],
     },
     {
       id: 4,
       score: 9,
-      text: "la quatrième question",
+      text: 'la quatrième question',
       resources: [
         {
-          "url_resource": "blabla",
-          "type_resource": "2"
+          url_resource: 'blabla',
+          type_resource: '1',
         },
         {
-          "url_resource": "blabla2",
-          "type_resource": "2"
-        }
+          url_resource: 'blabla2',
+          type_resource: '1',
+        },
       ],
     },
     {
       id: 5,
       score: 6,
-      text: "la sixième question",
+      text: 'la cinquième question',
       resources: [
         {
-          "url_resource": "blabla",
-          "type_resource": "1"
+          url_resource: 'blabla',
+          type_resource: '1',
         },
         {
-          "url_resource": "blabla2",
-          "type_resource": "3"
-        }
+          url_resource: 'blabla2',
+          type_resource: '1',
+        },
       ],
     },
     {
       id: 6,
       score: 8,
-      text: "la sixième question",
+      text: 'la sixième question',
       resources: [
         {
-          "url_resource": "blabla",
-          "type_resource": "1"
+          url_resource: 'blabla',
+          type_resource: '1',
         },
         {
-          "url_resource": "blabla2",
-          "type_resource": "1"
-        }
+          url_resource: 'blabla2',
+          type_resource: '1',
+        },
       ],
     },
-  ]
+  ];
 
   // Fonction qui permet de trier les objets du tableau par ordre de score croissant
   const getQuestions = () => {
-    const getMinScore = answers.sort((a, b) => {
-      return a.score - b.score;
-    })
-    return [getMinScore[0], getMinScore[1], getMinScore[2]]
-  }
+    const getMinScore = answers.sort((a, b) => a.score - b.score);
+    return [getMinScore[0], getMinScore[1], getMinScore[2]];
+  };
 
-    const getVideoResource = (resources) => {
-      return resources.filter((value) => {
-        return value.type_resource === "1"
-      })
-    }
+  const getVideoResource = resources => resources.filter(value => value.type_resource === '1');
 
-    const getBookResource = (resources) => {
-      return resources.filter((value) => {
-        return value.type_resource === "2"
-      })
-    }
+  const getBookResource = resources => resources.filter(value => value.type_resource === '2');
 
-    const getMusicResource = (resources) => {
-      return resources.filter((value) => {
-        return value.type_resource === "3"
-      })
-    }
+  const getMusicResource = resources => resources.filter(value => value.type_resource === '3');
 
-  const [resource, setResource] = useState([])
-
-
-
+  const [resource, setResource] = useState([]);
   return (
     <div className="container-result">
-      <h5> Voici les 3 forces que vous pouvez développer dès à présent. <br /><br />Cliquez sur une des forces pour commencer le programme.</h5>
+      <h5>
+        Voici les 3 forces que vous pouvez développer dès à présent.
+        <br />
+        <br />
+        Cliquez sur une des forces pour commencer le programme.
+      </h5>
       <div className="card-results">
         <ul className="list-group list-group-flush">
-          {getQuestions().map((question) => {  //récupère les 3 questions dont les scores sont les + bas
-            return <li onClick={() => {
-              setResource(question.resources);
-              getVideoResource(question.resources);
-            }} className="list-group-item" style={{ cursor: "pointer" }} data-toggle="modal" data-target="#exampleModal" >{question.text}</li>
-          })}
+          {getQuestions().map(question => (
+            <li
+              onClick={() => {
+                setResource(question.resources);
+                getVideoResource(question.resources);
+              }}
+              // eslint-disable-next-line jsx-a11y/no-noninteractive-element-to-interactive-role
+              role="button"
+              className="list-group-item"
+              style={{ cursor: 'pointer' }}
+              data-toggle="modal"
+              data-target="#exampleModal"
+            >
+              {question.text}
+            </li>
+          ))}
         </ul>
       </div>
       <div>
-        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" isOpen>
-          <div class="modal-dialog" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Votre programme</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" isOpen>
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">Votre programme</h5>
+                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">&times;</span>
                 </button>
               </div>
-              <div class="modal-body">
+              <div className="modal-body">
                 <div>
                   <h6>A voir</h6>
                   <ul>
-                    {getVideoResource(resource).map((toto) => {
-                      return <li>{toto.url_resource}</li>
-                    })}
+                    {getVideoResource(resource).map(res => <li>{res.url_resource}</li>)
+                    }
                   </ul>
                 </div>
                 <div>
                   <h6>A lire</h6>
                   <ul>
-                  {getBookResource(resource).map((toto) => {
-                      return <li>{toto.url_resource}</li>
-                    })}
+                    {getBookResource(resource).map(res => <li>{res.url_resource}</li>)}
                   </ul>
                 </div>
                 <div>
                   <h6>A écouter</h6>
                   <ul>
-                  {getMusicResource(resource).map((toto) => {
-                      return <li>{toto.url_resource}</li>
-                    })}
+                    {getMusicResource(resource).map(res => <li>{res.url_resource}</li>)}
+
                   </ul>
                 </div>
               </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-
-  )
+  );
 };
 
 
