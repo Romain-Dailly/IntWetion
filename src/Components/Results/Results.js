0a13d02e0cdc92b662cb67c65463e3fe2045c5eb
Anais <anais.chevallier@gmail.com>
2019-07-22 16:52:05 +0200

    ES LINT OK

--- src/Components/Results/Results.js
+++ src/Components/Results/Results.js
@@ -1,15 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
-
 import './Results.css';
-
-// import { returnStatement } from '@babel/types';
-// import { log } from 'util';
-
-// Appel du state via le store redux
-// const Results = () => {
-
-// const [modal, setModal] = useState(false);
 
 const Results = () => {
   const selectData = useSelector(store => store.card.results);
@@ -20,27 +11,24 @@ const Results = () => {
     const getMinScore = dataSort.sort((a, b) => a.answer - b.answer);
     return [getMinScore[0], getMinScore[1], getMinScore[2]];
   };
-  const getVideoResource = resources =>
-    resources.filter(value => value.type_resource === 2);
+  const getVideoResource = resources => resources.filter(value => value.type_resource === 2);
 
-  const getBookResource = resources =>
-    resources.filter(value => value.type_resource === 1);
+  const getBookResource = resources => resources.filter(value => value.type_resource === 1);
 
-  const getMusicResource = resources =>
-    resources.filter(value => value.type_resource === 3);
+  const getMusicResource = resources => resources.filter(value => value.type_resource === 3);
 
   const [resource, setResource] = useState([]);
-    
+
   return (
-    <div className='container-result'>
+    <div className="container-result">
       <h5>
         Voici les 3 forces que vous pouvez développer dès à présent.
         <br />
         <br />
         Cliquez sur une des forces pour commencer le programme.
       </h5>
-      <div className='card-results'>
-        <ul className='list-group list-group-flush'>
+      <div className="card-results">
+        <ul className="list-group list-group-flush">
           {getQuestions().map(question => (
             <li
               onClick={() => {
@@ -48,11 +36,11 @@ const Results = () => {
                 // getVideoResource(question.question.resources);
               }}
               // eslint-disable-next-line jsx-a11y/no-noninteractive-element-to-interactive-role
-              role='button'
-              className='list-group-item'
+              role="button"
+              className="list-group-item"
               style={{ cursor: 'pointer' }}
-              data-toggle='modal'
-              data-target='#exampleModal'
+              data-toggle="modal"
+              data-target="#exampleModal"
             >
               {question.question.text_question}
             </li>
@@ -61,30 +49,30 @@ const Results = () => {
       </div>
       <div>
         <div
-          className='modal fade'
-          id='exampleModal'
-          tabIndex='-1'
-          role='dialog'
-          aria-labelledby='exampleModalLabel'
-          aria-hidden='true'
+          className="modal fade"
+          id="exampleModal"
+          tabIndex="-1"
+          role="dialog"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
           isOpen
         >
-          <div className='modal-dialog' role='document'>
-            <div className='modal-content'>
-              <div className='modal-header'>
-                <h5 className='modal-title' id='exampleModalLabel'>
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">
                   Votre programme
                 </h5>
                 <button
-                  type='button'
-                  className='close'
-                  data-dismiss='modal'
-                  aria-label='Close'
+                  type="button"
+                  className="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
                 >
-                  <span aria-hidden='true'>&times;</span>
+                  <span aria-hidden="true">&times;</span>
                 </button>
               </div>
-              <div className='modal-body'>
+              <div className="modal-body">
                 <div>
                   <h6>A voir</h6>
                   <ul>
@@ -110,11 +98,11 @@ const Results = () => {
                   </ul>
                 </div>
               </div>
-              <div className='modal-footer'>
+              <div className="modal-footer">
                 <button
-                  type='button'
-                  className='btn btn-secondary'
-                  data-dismiss='modal'
+                  type="button"
+                  className="btn btn-secondary"
+                  data-dismiss="modal"
                 >
                   Fermer
                 </button>
