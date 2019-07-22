657336a2784a8c4a8cb2267a03094a03bc5aaf12
Anais <anais.chevallier@gmail.com>
2019-07-22 16:56:09 +0200

    ES LINT OK

--- src/data/source.js
+++ src/data/source.js
@@ -108,16 +108,13 @@ const fetchDetailedCards = async (callback) => {
 
 // Delete all from a card with its id
 const deleteCard = async (id, callback) => {
-  const params = { id : `${id}` };
-  console.log(params);
-  
+  const params = { id: `${id}` };
   const path = 'card';
   const deleted = await deleteData(path, params);
   if (deleted.status === 200) {
     callback();
-  }else{
-    console.log("Error");
-    
+  } else {
+    console.log('Error');
   }
 };
 
