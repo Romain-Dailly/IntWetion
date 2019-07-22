8b61572e730d7e0e1f7a4208781371d19414626b
Anais <anais.chevallier@gmail.com>
2019-07-22 16:30:06 +0200

    final cleaning - eslint

--- src/Components/ContextToolBar/ContextToolBar.js
+++ src/Components/ContextToolBar/ContextToolBar.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 const ContextToolBar = ({ title, handleClick }) => (
   <div className="context-tool-bar background-white">
-   <i className="logo icon-alt icon-lotus" />
-     <p className="header-5 m-0">{title}</p> 
+    <i className="logo icon-alt icon-lotus" />
+    <p className="header-5 m-0">{title}</p>
     <i tabIndex="-1" role="button" onClick={handleClick} className="icon icon-close" />
   </div>
 );
+
 export default ContextToolBar;
